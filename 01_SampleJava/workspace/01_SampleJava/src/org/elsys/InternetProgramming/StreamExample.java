e7262b03948b86b3758e633453c76889e759d7d6
Emil Dudev <emildudev@gmail.com>
2014-09-23 15:00:59 +0000

    Input stream reader

--- 01_SampleJava/workspace/01_SampleJava/src/org/elsys/InternetProgramming/StreamExample.java
+++ 01_SampleJava/workspace/01_SampleJava/src/org/elsys/InternetProgramming/StreamExample.java
@@ -1,11 +1,16 @@
 package org.elsys.InternetProgramming;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class StreamExample {
-	public static void main(String []args) {
-		
+	public final static String END_READER = "END";
+	
+	public static void main(String []args) throws IOException {
+		//inputStreamExample();
+		bufferedReaderExample();
 	}
 	
 	public static void inputStreamExample() throws IOException {
@@ -18,5 +23,20 @@ public class StreamExample {
 		final byte a[] = new byte[3];
 		input.read(a);
 		System.out.println(new String(a));
+	}
+	
+	public static void bufferedReaderExample() throws IOException {
+		final InputStream input = System.in;
+		final InputStreamReader inputStreamReader = new InputStreamReader(input);
+		final BufferedReader reader = new BufferedReader(inputStreamReader);
+		
+		final String wholeLine = reader.readLine();
+		System.out.println(wholeLine);
+		
+		int count = 0;
+		while(!reader.readLine().equals(END_READER)) {
+			++count;
+		}
+		System.out.println("You have entered: " + count + " lines.");
 	}
 }
